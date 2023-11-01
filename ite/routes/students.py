164de08346eafead211bf075f7474c5eccac78a1
Vinicius Lucchesi <viniciuslucchesi.ti@gmail.com>
2023-11-01 20:21:13 -0300

    Schemas applied

--- ite/routes/students.py
+++ ite/routes/students.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from ite.config.database import mongo
-from ite.models.student import Student, UpdateStudent
+from ite.schemas.students import student_list_schema, student_schema
+from ite.models.student import Student
 
 
 router = APIRouter()
@@ -11,19 +12,17 @@ router = APIRouter()
 @router.get("/list")
 async def students_list():
     """Return a list of students."""
-    students: list[Student] = []
+    students = []
     try:
         all_students = await mongo.students.find().to_list(None)
-        for student in all_students:
-            students.append(Student(**student))
+        students = student_list_schema(all_students)
     except Exception as error:
         return JSONResponse(content={"error": str(error)}, status_code=500)
     
     if len(students) == 0:
         return JSONResponse(content={"msg": "There is no Students in the database"}, status_code=200)
     
-    data = [student.model_dump() for student in students]
-    return JSONResponse(content=data, status_code=200)
+    return JSONResponse(content=students, status_code=200)
 
 
 @router.get("/detail/{student_ra}")
@@ -54,11 +53,20 @@ async def student_create(student: Student):
 
 
 @router.put('/update/{student_ra}')
-async def student_update(student_ra: str, student: UpdateStudent):
+async def student_update(student_ra: str, student: Student):
     """Update a student based on its RA."""
-    student = student.model_dump()
     try:
-        await mongo.students.update_one({"ra": student_ra}, {"$set": student})
+        student = dict(student)
+
+        if student["email"] is None:
+            del student["email"]
+        if student["course"] is None:
+            del student["course"]
+
+        await mongo.students.find_one_and_update(
+            {"ra": student_ra},
+            {"$set": student}
+        )
     except Exception as error:
         return JSONResponse(content={"error": str(error)}, status_code=500)
     
@@ -69,7 +77,7 @@ async def student_update(student_ra: str, student: UpdateStudent):
 async def student_delete(student_ra: str):
     """Delete a student based on its RA."""
     try:
-        await mongo.students.delete_one({"ra": student_ra})
+        await mongo.students.find_one_and_delete({"ra": student_ra})
     except Exception as error:
         return JSONResponse(content={"error": str(error)}, status_code=500)
     
